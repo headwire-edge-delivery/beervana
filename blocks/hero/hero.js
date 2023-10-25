0d53e9365f725f2850bb35e8a9c003791cff1dfd
Joshua Zeltman <joshua.zeltman@gmail.com>
2023-10-25 16:34:29 +0000

    formatting
    hero styling
    
    todo: fix bug in css

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,7 @@
 export default async function decorate(block) {
   console.log("hero decorate", block);
+  const image = block.querySelector("picture");
+  const title = block.querySelector("h1");
+
+  block.innerHTML = `${title.outerHTML}${image.outerHTML}`;
 }
