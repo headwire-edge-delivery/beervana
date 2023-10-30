e0663a65031c9dd6e4b11d1029f4ca828635af0a
Joshua Zeltman <joshua.zeltman@gmail.com>
2023-10-30 11:28:52 -0300

    refactored team members to use correct block formatting in table

--- blocks/team/team.js
+++ blocks/team/team.js
@@ -1,40 +1,21 @@
 export default async function decorate(block) {
-  console.log("team decorate", block);
-  const teamMembers = [{}];
-  let teamMemberIndex = 0;
-  block.querySelectorAll(".team div > div > *").forEach((el, index) => {
-    if (el.tagName === "HR") { 
-        teamMemberIndex++; 
-        teamMembers.push({})
-        return; 
-    }
-    else {
-        if (el.querySelector("picture")) {
-            teamMembers[teamMemberIndex]["picture"] = el.querySelector("picture");
-        } else {
-            teamMembers[teamMemberIndex][el.tagName] = el;
-        }
-    }
-    console.log("team els", el);
-  });
+  block.querySelectorAll(":scope > div")?.forEach((el, index) => {
+    el.classList.add("team-member");
+    const teamMemberPhoto = el.querySelector("picture");
+    teamMemberPhoto.classList.add("team-member-photo");
 
-  console.log("teamMembers", teamMembers);
-  block.innerHTML = "";
-  console.log("block", block);
-  teamMembers.forEach((teamMember, index) => {
-    const teamMemberBlock = document.createElement("div");
-    teamMemberBlock.classList.add("team-member");
     const teamMemberInfo = document.createElement("div");
     teamMemberInfo.classList.add("team-member-info");
-    teamMemberBlock.appendChild(teamMember.picture);
+    const teamMemberName = el.querySelector("h3");
+    const teamMemberPosition = el.querySelector("h4");
+    const teamMemberDescription = el.querySelector("p:last-child");
 
-    teamMemberInfo.appendChild(teamMember.H3);
-    teamMemberInfo.appendChild(teamMember.H4);
-    teamMemberInfo.appendChild(teamMember.P);
-    teamMemberBlock.appendChild(teamMemberInfo);
-    
-    block.appendChild(teamMemberBlock);
+    if (teamMemberName) teamMemberInfo.appendChild(teamMemberName);
+    if (teamMemberPosition) teamMemberInfo.appendChild(teamMemberPosition);
+    if (teamMemberDescription) teamMemberInfo.appendChild(teamMemberDescription);
+
+    el.innerHTML = "";
+    el.appendChild(teamMemberPhoto);
+    el.appendChild(teamMemberInfo);
   });
-
 }
-
