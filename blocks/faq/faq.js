f7a1350c94d6601ffa0a3732c137963a9bdb401d
Joshua Zeltman <joshua.zeltman@gmail.com>
2023-10-30 11:19:10 -0300

    styled the faqs

---
+++ blocks/faq/faq.js
@@ -0,0 +1,11 @@
+export default async function decorate(block) {
+  block.querySelectorAll(":scope > div")?.forEach((el, index) => {
+    el.classList.add("faq-item-wrapper");
+    const faqMarkup = el.querySelector(":scope > div");
+    faqMarkup.classList.add("faq-item");
+    if (!faqMarkup.querySelector("p")) {
+      el.remove();
+    }
+    console.log("faq els", el);
+  });
+}
