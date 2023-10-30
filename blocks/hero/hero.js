2944a8d57666900036fdae0df7e0f77e34b7b161
Joshua Zeltman <joshua.zeltman@gmail.com>
2023-10-30 15:21:28 -0300

    refactored hero component
    adjusted styling on team fragment
    
    finished faq page

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,8 +1,3 @@
 export default async function decorate(block) {
-  console.log("hero decorate", block);
-  const image = block.querySelector("picture");
-  const title = block.querySelector("h1");
-
-  block.innerHTML = `${title.outerHTML}${image.outerHTML}`;
-  document.body.classList.add("has-hero");
+  block.querySelector(":scope div > div")?.classList?.add("hero-content");
 }
