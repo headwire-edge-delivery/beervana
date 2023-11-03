f1a96c1ebf832caa70a99a00d86787378c608f7b
Joshua Zeltman <joshua.zeltman@gmail.com>
2023-11-03 12:11:00 -0300

    reverting attempted fix for improperly sized hero image

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,15 +1,8 @@
-import { createOptimizedPicture } from "../../scripts/aem.js";
-
 export default async function decorate(block) {
   block.querySelector(":scope div > div")?.classList?.add("hero-content");
   const picture = block.querySelector(":scope picture");
   if (picture) {
     picture.parentNode.replaceWith(picture);
     document.body.classList.add("has-hero");
-    const src = picture.querySelector("img").getAttribute("src")
-    if (src) {
-      const optimizedPicture = createOptimizedPicture(src);
-      picture.replaceWith(optimizedPicture);
-    }
   }
 }
