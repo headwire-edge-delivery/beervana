9708f469028a10e680115ca8a0aaf6969bae701b
Joshua Zeltman <joshua.zeltman@gmail.com>
2023-11-03 10:23:00 -0300

    updates to hero image for testing
    adjusted image size

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,8 +1,15 @@
+import { createOptimizedPicture } from "../../scripts/aem.js";
+
 export default async function decorate(block) {
   block.querySelector(":scope div > div")?.classList?.add("hero-content");
   const picture = block.querySelector(":scope picture");
   if (picture) {
     picture.parentNode.replaceWith(picture);
     document.body.classList.add("has-hero");
+    const src = picture.querySelector("img").getAttribute("src")
+    if (src) {
+      const optimizedPicture = createOptimizedPicture(src);
+      picture.replaceWith(optimizedPicture);
+    }
   }
 }
