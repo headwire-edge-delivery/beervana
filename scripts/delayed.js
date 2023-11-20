9001c7ceb4cbb65dbdd441bf212fb6f541741d4a
Joshua Zeltman <joshuazeltman@Joshuas-MacBook-Pro.local>
2023-11-20 10:11:56 +0100

    added interstitial page and external link handling

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,12 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+function handleExternalLinks() {
+  document.querySelectorAll('a:not(.interstitial a.button)').forEach(link => {
+    if (link.host !== window.location.host && link.href !== '#back' && link.href !== '#redirect') {
+      link.href = `${window.location.origin}/external-link-interstitial?redirect=${link.href}&back=${window.location.href}`
+    }
+  });
+}
+
+handleExternalLinks();
