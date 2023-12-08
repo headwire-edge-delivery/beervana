aa5f4a3f1d30c290524f26020c5467b54a65b0e0
Joshua Zeltman <joshua.zeltman@gmail.com>
2023-12-08 09:39:55 +0100

    added basic breakout template
    refactored templating mechanism

--- blocks/include/include.js
+++ blocks/include/include.js
@@ -20,6 +20,30 @@ function templateCard({ path, title, description }) {
   </li>`;
 }
 
+function templateBreakoutElements({ path, title, description }, index) {
+  console.log('templateBreakoutElements', path, title, description, index);
+  return `<div class="breakout-${index % 2 === 0 ? 'left' : 'right'} two-columns block">
+    <div class="default-content-wrapper">
+      <h2 id="portland-art-museum">Portland Art Museum</h2>
+      <p>The Portland Art Museum has 240,000 square feet, with more than 112,000 square feet of gallery space. The museum’s permanent collection has over 42,000 works of art</p>
+      <ul>
+        <li><span class="icon icon-map-pin"><img data-icon-name="map-pin" src="/icons/map-pin.svg" loading="lazy" alt="map-pin" width="16" height="16"></span>1219 SW Park Avenue<br>Portland, OR 97205</li>
+        <li><span class="icon icon-phone"><img data-icon-name="phone" src="/icons/phone.svg" loading="lazy" alt="phone" width="16" height="16"></span>General info: 503-226-2811</li>
+        <li><span class="icon icon-phone"><img data-icon-name="phone" src="/icons/phone.svg" loading="lazy" alt="phone" width="16" height="16"></span>Membership: 503-276-4249</li>
+        <li><span class="icon icon-link"><img data-icon-name="link" src="/icons/link.svg" loading="lazy" alt="link" width="16" height="16"></span><a href="http://localhost:3000/external-link-interstitial?redirect=https://portlandartmuseum.org/&amp;back=http://localhost:3000/activities" title="https://portlandartmuseum.org/">https://portlandartmuseum.org/</a></li>
+      </ul>
+    </div>
+    <div class="default-content-wrapper image-content">
+      <picture>
+        <source type="image/webp" srcset="./media_1ddaedc26276f9520db5a059733923dcbdb8cff5e.png?width=2000&amp;format=webply&amp;optimize=medium" media="(min-width: 600px)">
+        <source type="image/webp" srcset="./media_1ddaedc26276f9520db5a059733923dcbdb8cff5e.png?width=750&amp;format=webply&amp;optimize=medium">
+        <source type="image/png" srcset="./media_1ddaedc26276f9520db5a059733923dcbdb8cff5e.png?width=2000&amp;format=png&amp;optimize=medium" media="(min-width: 600px)">
+        <img loading="lazy" alt="" src="./media_1ddaedc26276f9520db5a059733923dcbdb8cff5e.png?width=750&amp;format=png&amp;optimize=medium" width="1133" height="934">
+      </picture>
+    </div>
+  </div>`;
+}
+
 function templateCards(block, data) {
   const cards = document.createElement('div');
   cards.classList.add('cards');
@@ -27,30 +51,46 @@ function templateCards(block, data) {
   block.appendChild(cards);
 }
 
+function templateDefault(block, data) {
+  const container = document.createElement('div');
+  container.classList.add('include-container');
+  container.innerHTML = `<ul>${data.map(templateCard).join('')}</ul>`;
+  block.appendChild(container);
+}
+
+function templateBreakout(block, data) {
+  const container = document.createElement('div');
+  container.classList.add('breakout-wrapper');
+  container.innerHTML = data.map(templateBreakoutElements).join('');
+  block.appendChild(container);
+}
+
+const templateConfig = {
+  cards: templateCards,
+  breakout: templateBreakout,
+  default: templateDefault,
+};
+
 export default async function decorate(block) {
   const queryA = block.querySelector('a[href*="query-index.json"]');
   if (queryA) {
     const style = Array.from(block.classList).filter((c) => c.toLowerCase() !== 'include' && c.toLowerCase() !== 'block')[0];
     const params = new URLSearchParams(queryA.href);
-    const buttonContainer = queryA.closest('.button-container');
-    buttonContainer.parentNode.remove();
+    block.querySelector(':scope > div:first-child').remove();
     const response = await fetch(queryA.href);
     if (response.ok) {
       let { data } = await response.json();
       if (data?.length > 0) {
+        /* refactor to loop over params and filter data */
         if (params.get('tag')) {
           data = data.filter((item) => JSON.parse(item.tags.toLowerCase()).includes(params.get('tag')));
         }
       }
 
-      // template
-      switch (style) {
-        case 'cards':
-          templateCards(block, data);
-          break;
-        default:
-          /* eslint-disable no-console */
-          console.log('default');
+      if (templateConfig[style]) {
+        templateConfig[style](block, data);
+      } else {
+        templateConfig.default(block, data);
       }
     }
   }
