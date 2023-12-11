3d95e17b5ce08d0a5565cb28427968e13d59e16b
Joshua Zeltman <joshua.zeltman@gmail.com>
2023-12-11 13:12:13 +0100

    various style tweaks on several pages for desktop

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,10 +1,12 @@
-export default function decorate(block) {
+import { loadBlocks } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
   // setup image columns
-  [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
+  [...block.children].forEach(async (row) => {
+    [...row.children].forEach(async (col) => {
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
@@ -12,6 +14,20 @@ export default function decorate(block) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
         }
+      }
+
+      const formLink = col.querySelector('.button-container:has(a[href$=".json"])');
+      if (formLink) {
+        const formContainer = document.createElement('div');
+        formContainer.classList.add('form', 'block');
+        formContainer.dataset.blockName = 'form';
+        formContainer.append(...formLink.childNodes);
+        formLink.before(formContainer);
+        formLink.remove();
+        await loadBlocks(formContainer.parentElement);
+        const section = formContainer.closest('.section');
+        section.removeAttribute('data-section-status');
+        section.removeAttribute('style');
       }
     });
   });
