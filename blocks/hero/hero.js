6c0b18eda4dbaaa80ed8373b1add0d29c3e1a80d
Joshua Zeltman <joshua.zeltman@gmail.com>
2023-12-19 12:40:41 +0100

    added colors, fonts, sizes, images from design
    styled hero, buttons
    
    started styling header

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -4,5 +4,17 @@ export default async function decorate(block) {
   if (picture) {
     picture.parentNode.replaceWith(picture);
     document.body.classList.add('has-hero');
+
+    const pictureWrapper = document.createElement('div');
+    pictureWrapper.classList.add('hero-picture');
+    pictureWrapper.appendChild(picture);
+    block.appendChild(pictureWrapper);
+
+    const heroContentWrapper = document.createElement('div');
+    heroContentWrapper.classList.add('hero-content-wrapper');
+    heroContentWrapper.append(...block.querySelectorAll('.hero-content *:not(.hero-picture, .hero-picture *)'));
+    block.prepend(heroContentWrapper);
+
+    block.querySelector(':scope > div:not(.hero-picture, .hero-content-wrapper)')?.remove();
   }
 }
